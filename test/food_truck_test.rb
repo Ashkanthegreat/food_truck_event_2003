366464fbc3aa7ab7251d00645825bffe584983c7
Ashkan Abbasi <airashkan@yahoo.com>
2020-04-15 10:15:07 -0600

    Create Food Truck class and tests.

---
+++ test/food_truck_test.rb
@@ -0,0 +1,23 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/food_truck'
+
+
+class FoodTruckTest < Minitest::Test
+
+  def setup
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
+
+  def test_it_exists
+    assert_instance_of FoodTruck, @food_truck
+  end
+
+  def test_food_truck_attributes
+    assert_equal 'Rocky Mountain Pies', @food_truck.name
+    assert_equal ({}), @food_truck.inventory
+
+  end
+end
